a6383d8f5ecf0593abe4ccf9aa746c1a491fe09c
Uhrendoktor <36703334+Uhrendoktor@users.noreply.github.com>
2026-08-29 19:10:26 +0200

    Fix Python workbench registration for FreeCAD GUI

--- InitGui.py
+++ InitGui.py
@@ -15,7 +15,15 @@ def _icon(name):
     return str(_ICON_DIR / name)
 
 
-class ClothPatternWorkbench:
+try:
+    import FreeCADGui as Gui
+except ImportError:
+    Gui = None
+
+_WorkbenchBase = Gui.Workbench if Gui is not None else object
+
+
+class ClothPatternWorkbench(_WorkbenchBase):
     MenuText = "Cloth Pattern"
     ToolTip = "Parametric sewing-pattern design"
     Icon = _icon("ClothPattern.svg")
@@ -30,21 +38,17 @@ class ClothPatternWorkbench:
         self.appendToolbar(self.MenuText, self.commands)
         self.appendMenu(self.MenuText, self.commands)
 
-    def Activated(self):
-        return None
-
-    def Deactivated(self):
-        return None
+    def Activated(self): return None
+    def Deactivated(self): return None
 
     def ContextMenu(self, recipient):
         if recipient in ("view", "tree") and self.commands:
             self.appendContextMenu(self.MenuText, self.commands)
 
-    def GetClassName(self):
-        return "Gui::PythonWorkbench"
+    def GetClassName(self): return "Gui::PythonWorkbench"
 
 
-class ClothSimulationWorkbench:
+class ClothSimulationWorkbench(_WorkbenchBase):
     MenuText = "Cloth Simulation"
     ToolTip = "3D cloth assembly and simulation"
     Icon = _icon("ClothSimulation.svg")
@@ -58,21 +62,17 @@ class ClothSimulationWorkbench:
         self.appendToolbar(self.MenuText, self.commands)
         self.appendMenu(self.MenuText, self.commands)
 
-    def Activated(self):
-        return None
-
-    def Deactivated(self):
-        return None
+    def Activated(self): return None
+    def Deactivated(self): return None
 
     def ContextMenu(self, recipient):
         if recipient in ("view", "tree") and self.commands:
             self.appendContextMenu(self.MenuText, self.commands)
 
-    def GetClassName(self):
-        return "Gui::PythonWorkbench"
+    def GetClassName(self): return "Gui::PythonWorkbench"
 
 
-class ClothSewingWorkbench:
+class ClothSewingWorkbench(_WorkbenchBase):
     MenuText = "Cloth Sewing"
     ToolTip = "Sewing operations and avatar fitting"
     Icon = _icon("ClothSewing.svg")
@@ -87,24 +87,15 @@ class ClothSewingWorkbench:
         self.appendToolbar(self.MenuText, self.commands)
         self.appendMenu(self.MenuText, self.commands)
 
-    def Activated(self):
-        return None
-
-    def Deactivated(self):
-        return None
+    def Activated(self): return None
+    def Deactivated(self): return None
 
     def ContextMenu(self, recipient):
         if recipient in ("view", "tree") and self.commands:
             self.appendContextMenu(self.MenuText, self.commands)
 
-    def GetClassName(self):
-        return "Gui::PythonWorkbench"
+    def GetClassName(self): return "Gui::PythonWorkbench"
 
-
-try:
-    import FreeCADGui as Gui
-except ImportError:
-    Gui = None
 
 if Gui is not None:
     Gui.addWorkbench(ClothPatternWorkbench())
