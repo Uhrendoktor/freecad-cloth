aa976a5abbb38e3ad67c72cf282d9b802f122ec6
Uhrendoktor <36703334+Uhrendoktor@users.noreply.github.com>
2026-08-30 10:44:16 +0200

    feat(avatar): register mannequin commands in Sewing workbench

--- InitGui.py
+++ InitGui.py
@@ -45,64 +45,44 @@ class _ClothWorkbench(_WorkbenchBase):
 
     def GetResources(self):
         """Return the standard FreeCAD workbench metadata contract."""
-        return {
-            "MenuText": self.MenuText,
-            "ToolTip": self.ToolTip,
-            "Icon": self.Icon,
-        }
+        return {"MenuText": self.MenuText, "ToolTip": self.ToolTip, "Icon": self.Icon}
 
-    def Activated(self):
-        return None
-
-    def Deactivated(self):
-        return None
+    def Activated(self): return None
+    def Deactivated(self): return None
 
     def ContextMenu(self, recipient):
         if recipient in ("view", "tree") and self.commands:
             self.appendContextMenu(self.MenuText, self.commands)
 
-    def GetClassName(self):
-        """Return FreeCAD's Python-workbench class identifier."""
-        return "Gui::PythonWorkbench"
+    def GetClassName(self): return "Gui::PythonWorkbench"
 
 
 class ClothPatternWorkbench(_ClothWorkbench):
-    MenuText = "Cloth Pattern"
-    ToolTip = "Parametric sewing-pattern design"
-    Icon = "ClothPattern.svg"
-
+    MenuText = "Cloth Pattern"; ToolTip = "Parametric sewing-pattern design"; Icon = "ClothPattern.svg"
     def Initialize(self):
-        if self.commands:
-            return
+        if self.commands: return
         import PatternCommands
         import PatternMarks
         self._register(PatternCommands.COMMANDS + PatternMarks.COMMANDS)
 
 
 class ClothSimulationWorkbench(_ClothWorkbench):
-    MenuText = "Cloth Simulation"
-    ToolTip = "3D cloth assembly and simulation"
-    Icon = "ClothSimulation.svg"
-
+    MenuText = "Cloth Simulation"; ToolTip = "3D cloth assembly and simulation"; Icon = "ClothSimulation.svg"
     def Initialize(self):
-        if self.commands:
-            return
+        if self.commands: return
         import SimulationCommands
         self._register(SimulationCommands.COMMANDS)
 
 
 class ClothSewingWorkbench(_ClothWorkbench):
-    MenuText = "Cloth Sewing"
-    ToolTip = "Sewing operations and avatar fitting"
-    Icon = "ClothSewing.svg"
-
+    MenuText = "Cloth Sewing"; ToolTip = "Sewing operations and avatar fitting"; Icon = "ClothSewing.svg"
     def Initialize(self):
-        if self.commands:
-            return
+        if self.commands: return
         import SewingCommands
         import SewingNetworkCommands
         import FittingCommands
-        self._register(SewingCommands.COMMANDS + SewingNetworkCommands.COMMANDS + FittingCommands.COMMANDS)
+        import AvatarCommands
+        self._register(SewingCommands.COMMANDS + SewingNetworkCommands.COMMANDS + FittingCommands.COMMANDS + AvatarCommands.COMMANDS)
 
 
 if Gui is not None:
