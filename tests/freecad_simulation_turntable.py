c564722313ec86d493692360ae751955129b29e3
Uhrendoktor <36703334+Uhrendoktor@users.noreply.github.com>
2026-09-14 20:45:52 +0200

    fix: offset turntable back pins by refined front mesh size

--- tests/freecad_simulation_turntable.py
+++ tests/freecad_simulation_turntable.py
@@ -257,7 +257,8 @@ def build_simulation_state(doc):
 
     fmesh, front_pins = local_boundary(front, front_outline)
     _, back_pins_local = local_boundary(back, back_outline)
-    back_pins = tuple(len(fmesh.vertices) + i for i in back_pins_local)
+    front_positions, _front_triangles, _front_boundary = __import__("freecad_cloth.simulation.SimulationMeshQuality", fromlist=["quality_piece_mesh"]).quality_piece_mesh(front, 0.0, scene.ParticleDistance)
+    back_pins = tuple(len(front_positions) + i for i in back_pins_local)
     scene.PinSelection = [str(i) for i in front_pins + back_pins]
     doc.recompute()
 
