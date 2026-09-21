f97b54dd97fd7c7e83cc2571d3faa7d6fb200a94
Uhrendoktor <36703334+Uhrendoktor@users.noreply.github.com>
2026-09-21 19:24:36 +0200

    test(sewing): make arc-length midpoint unambiguous

--- tests/test_seam_graph.py
+++ tests/test_seam_graph.py
@@ -58,7 +58,7 @@ def test_arc_length_stitch_mapping_ignores_nonuniform_boundary_vertex_density():
     value = graph()
     edges = {("left", 1): (10, 11, 12, 13), ("right", 3): (20, 21, 22)}
     points = {
-        ("left", 1): ((0.0, 0.0), (1.0, 0.0), (9.0, 0.0), (10.0, 0.0)),
+        ("left", 1): ((0.0, 0.0), (1.0, 0.0), (3.0, 0.0), (10.0, 0.0)),
         ("right", 3): ((0.0, 0.0), (5.0, 0.0), (10.0, 0.0)),
     }
     assert value.stitch_pairs(edges, edge_points=points) == ((10, 20), (12, 21), (13, 22))
