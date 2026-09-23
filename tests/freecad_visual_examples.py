4217912309e55fbb463cdebf8bf53b4d60a04b51
Uhrendoktor <36703334+Uhrendoktor@users.noreply.github.com>
2026-09-23 11:45:46 +0200

    test: reduce blanket launch clearance

--- tests/freecad_visual_examples.py
+++ tests/freecad_visual_examples.py
@@ -115,7 +115,7 @@ def main():
     try:
         sketch, outline = make_rectangle_sketch(doc, "BlanketSketch", 260.0, 260.0)
         piece = adopt_sketch(doc, sketch)
-        placement = App.Placement(App.Vector(-130.0, -130.0, 150.0), App.Rotation())
+        placement = App.Placement(App.Vector(-130.0, -130.0, 70.0), App.Rotation())
         piece.Placement = placement
         piece.Sketch.Placement = placement
 
