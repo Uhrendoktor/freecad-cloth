1d889a72890f636af8a620f27a04bbaba93776ec
Uhrendoktor <36703334+Uhrendoktor@users.noreply.github.com>
2026-09-26 01:00:37 +0200

    fix: retain deferred sewing dialog close timer

--- freecad_cloth/sewing/SewingCreationGui.py
+++ freecad_cloth/sewing/SewingCreationGui.py
@@ -280,7 +280,11 @@ class SewingCreationTaskPanel:
             from PySide import QtCore
         except ImportError:
             from PySide2 import QtCore
-        QtCore.QTimer.singleShot(0, _close_active_task_dialog)
+        timer = QtCore.QTimer()
+        timer.setSingleShot(True)
+        timer.timeout.connect(_close_active_task_dialog)
+        self._close_timer = timer
+        timer.start(0)
 
     def accept(self):
         try:
