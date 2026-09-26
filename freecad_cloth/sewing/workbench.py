b869fbca7016a2b2c58a4232f8bee918a1137791
Uhrendoktor <36703334+Uhrendoktor@users.noreply.github.com>
2026-09-26 11:42:26 +0200

    seams: refresh colors on Sewing workbench activation

--- freecad_cloth/sewing/workbench.py
+++ freecad_cloth/sewing/workbench.py
@@ -39,6 +39,10 @@ class ClothSewingWorkbench(ClothWorkbenchBase):
         super().__init__()
         self.Icon = str(Path(__file__).resolve().parents[2] / "resources" / "icons" / "ClothSewing.svg")
 
+    def Activated(self):
+        from freecad_cloth.sewing.SewingView import refresh_seam_colors
+        refresh_seam_colors()
+
     def Initialize(self):
         if self.commands:
             return
